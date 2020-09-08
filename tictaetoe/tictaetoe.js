6d135f5d83791092bc79c8ba6638a9bad1b3dd26
dianaatorress <dianaatorress@users.noreply.github.com>
2020-09-08 18:31:35 -0500

    cambios

--- tictaetoe/tictaetoe.js
+++ tictaetoe/tictaetoe.js
@@ -1,6 +1,17 @@
-console.log("hola")
-function marcar() {
-  let casilla = document.getElementById("casilla1");
-  casilla.classList.add("casillaMorada");
-  casilla.childNodes[0].innerText = "X"
+console.log("Adhara Elven")
+let JugadorUnoTurno = true
+
+function colorear(numero) {
+  let casilla = document.getElementById("casilla" + numero);
+  casilla.classList.add("casillaJade");
+  casilla.classList.remove("casillaDorada");
+  if(JugadorUnoTurno){casilla.childNodes[0].innerText = "X";
+  JugadorUnoTurno = false
+}
+  else{
+      casilla.classList.add("casillaDorada")
+      casilla.classList.remove("casillaJade");
+      casilla.childNodes[0].innerText = "0";
+      JugadorUnoTurno = true
+  }
 }
