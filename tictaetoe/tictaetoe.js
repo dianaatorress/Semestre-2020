8f492a081e4a48e1fec4e0d6aab547bfd0fa659a
dianaatorress <dianaatorress@users.noreply.github.com>
2020-09-10 18:04:53 -0500

    wuju!, ya casi

--- tictaetoe/tictaetoe.js
+++ tictaetoe/tictaetoe.js
@@ -3,15 +3,56 @@ let JugadorUnoTurno = true
 
 function colorear(numero) {
   let casilla = document.getElementById("casilla" + numero);
-  casilla.classList.add("casillaJade");
-  casilla.classList.remove("casillaDorada");
-  if(JugadorUnoTurno){casilla.childNodes[0].innerText = "X";
-  JugadorUnoTurno = false
-}
-  else{
-      casilla.classList.add("casillaDorada")
-      casilla.classList.remove("casillaJade");
-      casilla.childNodes[0].innerText = "0";
-      JugadorUnoTurno = true
+
+
+  let conTexto = ocupada(casilla)
+
+  if(!conTexto){
+    if(JugadorUnoTurno){
+      casilla.classList.add("casillaJade");
+      casilla.classList.remove("casillaDorada");
+      casilla.childNodes[0].innerText = "X";
+    JugadorUnoTurno = false
   }
+    else{
+        casilla.classList.add("casillaDorada")
+        casilla.classList.remove("casillaJade");
+        casilla.childNodes[0].innerText = "0";
+        JugadorUnoTurno = true
+    }
+  }
+
+  }
+
+
+
+function ocupada(casilla) {
+  if(casilla.childNodes[0].innerText){
+    console.log("sí está ocupada");
+    return true;
+  }
+  else {
+    console.log("no está ocupada");
+    return false;
+  }
+
+}
+
+function volvi() {
+  limcas(1);
+  limcas(2);
+  limcas(3);
+  limcas(4);
+  limcas(5);
+  limcas(6);
+  limcas(7);
+  limcas(8);
+  limcas(9);
+}
+
+function limcas(numero) {
+  let casilla = document.getElementById("casilla" + numero);
+  casilla.childNodes[0].innerText = "";
+  casilla.classList.remove("casillaJade");
+  casilla.classList.remove("casillaDorada");
 }
