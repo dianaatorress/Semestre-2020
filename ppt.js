c79e28cb4046830f9fcf58daff399d73457ca46b
dianaatorress <dianaatorress@users.noreply.github.com>
2020-11-03 18:19:54 -0600

    Detalles de diseño

--- ppt.js
+++ ppt.js
@@ -7,22 +7,24 @@ console.log("Bienvenido al juego de piedra, papel o tijera");
 //cambiar numero que eligio el cpu por palabras o la imagen
 let puntosUsuario = 0;
 let puntoCPU = 0;
-function jueUsuario(opUsuario){
-  console.log(opUsuario);
-  let eleAleatorio = Math.floor(Math.random() * 3) + 1;
-  console.log(eleAleatorio);
-  document.getElementById("eleccion").innerText = "el cpu eligio " + eleAleatorio;
-  if (opUsuario == eleAleatorio) {
-    document.getElementById("mensaje").innerText="EMPATE!!!"
-  } else if (
-    opUsuario == 1 && eleAleatorio == 3
-    || opUsuario == 2 && eleAleatorio == 1
-    || opUsuario == 3 && eleAleatorio == 2
-  ) {
-    document.getElementById("mensaje").innerText = "FELICIDADES USUARIO!!!"
-    document.getElementById("pjugador").innerText = puntosUsuario += 1;
-  } else {
-    document.getElementById("mensaje").innerText = "FELICIDADES CPU!!!"
-    document.getElementById("pcpu").innerText = puntoCPU += 1;
+
+  function jueUsuario(opUsuario){
+    console.log(opUsuario);
+    let eleAleatorio = Math.floor(Math.random() * 3) + 1;
+    console.log(eleAleatorio);
+    document.getElementById("eleccion").style.display = "block";
+    document.getElementById("eleccioon").innerText = eleAleatorio;
+    if (opUsuario == eleAleatorio) {
+      document.getElementById("mensaje").innerText="EMPATE!!!"
+    } else if (
+      opUsuario == 1 && eleAleatorio == 3
+      || opUsuario == 2 && eleAleatorio == 1
+      || opUsuario == 3 && eleAleatorio == 2
+    ) {
+      document.getElementById("mensaje").innerText = "FELICIDADES USUARIO!!!"
+      document.getElementById("pjugador").innerText = puntosUsuario += 1;
+    } else {
+      document.getElementById("mensaje").innerText = "FELICIDADES CPU!!!"
+      document.getElementById("pcpu").innerText = puntoCPU += 1;
+    }
   }
-}
